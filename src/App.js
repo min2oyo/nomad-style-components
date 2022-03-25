f99d858a457c2e3238e5fdb8f0456492effa5ebe
min2oyo <min2u@outlook.com>
2022-03-25 11:57:33 +0900

    01_셋업

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button danger />
+      <Button />
+    </>
   );
 }
+
+const Button = ({ danger }) => (
+  <button className={danger ? "button button--danger" : "button button--success"}>Hello</button>
+)
 
 export default App;
