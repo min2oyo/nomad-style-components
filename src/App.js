95f62e7b16b0fea52647b7d46406f94c565be45c
min2oyo <min2u@outlook.com>
2022-03-25 13:07:23 +0900

    02_Hello World with Styled Components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import "./App.css";
+import styled from "styled-components";
 
 function App() {
   return (
     <>
-      <Button danger />
-      <Button />
+      <Container>
+        <Button>Hello</Button>
+        <Button danger>Hello</Button>
+      </Container>
     </>
   );
 }
 
-const Button = ({ danger }) => (
-  <button className={danger ? "button button--danger" : "button button--success"}>Hello</button>
-)
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: pink;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
+`;
+
+// const Button = ({ danger }) => (
+//   <button className={danger ? "button button--danger" : "button button--success"}>Hello</button>
+// )
 
 export default App;
