19d9685c7600b98dbbf11843af5ff86dd077cbb5
min2oyo <min2u@outlook.com>
2022-03-25 17:18:46 +0900

    04_Animations

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, css, keyframes } from "styled-components";
 
-createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
   body {
-    padding: 0;
+    padding: 200;
     margin: 100;
   }
-`;
+`
 
 function App() {
   return (
     <>
       <Container>
         <Button>Hello</Button>
-        <Button danger>Hello</Button>
+        <Button danger rotationTime={5}>Hello</Button>
         <Anchor href="http://google.com">Go to google</Anchor>
+        <GlobalStyle />
       </Container>
     </>
   );
@@ -23,7 +24,7 @@ const Container = styled.div`
   height: 100vh;
   width: 100%;
   background-color: pink;
-`;
+`
 
 const Button = styled.button`
   border-radius: 50px;
@@ -38,10 +39,24 @@ const Button = styled.button`
     outline: none;
   }
   background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
-`;
+  ${props => {
+    if (props.danger) {
+      return css`animation: ${rotation} ${props.rotationTime}s linear infinite`;
+    }
+  }}
+`
 
 const Anchor = styled(Button.withComponent("a"))`
   text-decoration: none;
-`;
+`
+
+const rotation = keyframes`
+  from {
+    transform: ratate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
 
 export default App;
